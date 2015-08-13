a20faecf1a2aee02ed0c1d8ec6c1cf4b15b54619
Dan Shur <danshur@gmail.com>
2015-08-13 01:10:56 -0700

    x
    x

---
+++ HelloWorld.java
@@ -0,0 +1,45 @@
+
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		/*
+		int inx = 200;
+		int jnx = 7;
+		int knx = inx % jnx;
+		System.out.println(knx);
+		
+		int inx = 200;
+		int jnx = 7;
+		int knx = jnx % inx;
+		System.out.println(knx);
+		
+		
+		int inx = 6;
+		int jnx = 7;
+		int knx = ++jnx + inx--;
+		System.out.println(knx);
+		
+		byte inx = 12;
+		byte jnx = 2;
+		byte knx = (byte) (inx >> jnx);
+		System.out.println(knx);		//3
+		
+		byte inx = 12;
+		byte jnx = 2;
+		byte knx = (byte) (inx << jnx);
+		System.out.println(knx);		//48
+		
+		byte inx = 12;
+		byte jnx = 3;
+		byte knx = (byte) (inx & jnx);
+		System.out.println(knx);		//0
+		*/
+		byte inx = 12;
+		byte jnx = 5;
+		byte knx = (byte) (inx | jnx);
+		System.out.println(knx);		//13
+		/**/
+	}
+	
+	
+}
